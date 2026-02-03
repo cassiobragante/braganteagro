7d4614a92b374e0a61ed9cf59a43ecd3b7e8ca1c
cassiobragante <cassiobragante@gmail.com>
2026-02-03 16:43:57 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-const CACHE_NAME = 'bragante-v1';
-
-// Arquivos que precisam ser cacheados para a instalação ser liberada
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './logo.png'
-      ]);
+    caches.open('v1').then((cache) => {
+      return cache.addAll(['./', './index.html', './manifest.json', './logo.png']);
     })
   );
 });
 
-// Responde as requisições mesmo sem internet
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
