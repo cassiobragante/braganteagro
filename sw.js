75e43080a2b41444a4516fdf68bfd5628e79d331
cassiobragante <cassiobragante@gmail.com>
2026-02-03 16:54:26 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,33 @@
+const CACHE_NAME = 'bragante-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './BackupSistemaDefinitivo.html',
+  './FormularioRacaoDefinitivo.html'
+];
+
+// Instala e salva os arquivos no cache do celular
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(['./', './index.html', './manifest.json', './logo.png']);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+  self.skipWaiting();
+});
+
+// Ativa e remove caches antigos
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
     })
   );
 });
 
+// Estratégia: Tenta internet primeiro. Se falhar, usa o cache (Offline)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
