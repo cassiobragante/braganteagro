e9ee87e50e4d778a85155e47e61958cf2795518d
cassiobragante <cassiobragante@gmail.com>
2026-02-03 16:35:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,10 @@
-const CACHE_NAME = 'bragante-agro-v2';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png',
-  'https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.17.1/firebase-database-compat.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js'
-];
+const CACHE_NAME = 'bragante-cache-v1';
+const assets = ['index.html', 'manifest.json'];
 
-// Instala e força o cache de todos os arquivos necessários
 self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-// Responde do cache primeiro, se não tiver, busca na rede
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
